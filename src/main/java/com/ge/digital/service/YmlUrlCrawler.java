4f91c62615262dcfcc1d88b2e5040d663365be94
Bismaya Mohapatra <bismaya.mohapatra@hashicorp.com>
2025-05-09 12:46:07 +0530

    Initial commit

---
+++ src/main/java/com/ge/digital/service/YmlUrlCrawler.java
@@ -0,0 +1,14 @@
+package com.ge.digital.service;
+
+import org.json.simple.JSONArray;
+
+import java.util.Map;
+
+public class YmlUrlCrawler implements Parser {
+
+    @Override
+    public Map<String, JSONArray> parse(String filePath, String startingNode) throws Exception {
+        //TODO : Future implementation for YML type
+        return null;
+    }
+}
